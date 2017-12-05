7074e975ec9eb5489fe315157025760a3339511e
ArbutusNate <arbutusnate@gmail.com>
2017-12-05 12:29:55 -0800

    b5 module ready to demo

--- client/src/components/BestOfFive/BestOfFive.js
+++ client/src/components/BestOfFive/BestOfFive.js
@@ -7,7 +7,16 @@ const BestOfFive = props =>
       <h2>Best 3 of 5 Tournament</h2>
     </div>
     <div className="b5-main">
-
+      <div className="b5-p1 b5-player">
+        <img src="images/turin_brand.jpg" alt="Turin's Youtube Brand" className="player-thumbnail"/>
+        <h3>JonTaun</h3>
+        <p className="b5-score">p1 wins</p>
+      </div>
+      <div className="b5-p2 b5-player">
+        <img src="images/turin_brand.jpg" alt="Turin's Youtube Brand" className="player-thumbnail"/>
+        <h3>Loremaster of Sotek</h3>
+        <p className="b5-score">p2 wins</p>
+      </div>
     </div>
   </div>
 
