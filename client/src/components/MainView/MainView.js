e7d10528ab437cae91882488f96fe5d7ea46cc36
ArbutusNate <arbutusnate@gmail.com>
2017-12-05 10:31:06 -0800

    minor social css tweaks

--- client/src/components/MainView/MainView.js
+++ client/src/components/MainView/MainView.js
@@ -9,6 +9,9 @@ class MainView extends Component {
     <div className="background">
       <Header/>
       <Social/>
+      <footer>
+        <div>Icons made by <a href="https://www.flaticon.com/authors/linh-pham" title="Linh Pham">Linh Pham</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0">CC 3.0 BY</a></div>
+      </footer>
     </div>
    )
   }
