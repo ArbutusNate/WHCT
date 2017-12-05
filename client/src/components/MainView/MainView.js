0d85e76e561b9ef03fe0b787dcd8f99c39beb158
ArbutusNate <arbutusnate@gmail.com>
2017-12-05 11:48:52 -0800

    added two modules for displaying tournament information

--- client/src/components/MainView/MainView.js
+++ client/src/components/MainView/MainView.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
 import Header from '../Header';
 import Social from '../Social';
+import TournamentView from '../TournamentView';
 import './MainView.css';
 
 class MainView extends Component {
   render () {
    return (
     <div className="background">
-      <Header/>
-      <Social/>
-      <footer>
-        <div>Icons made by <a href="https://www.flaticon.com/authors/linh-pham" title="Linh Pham">Linh Pham</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0">CC 3.0 BY</a></div>
-      </footer>
+      <Header />
+      <Social />
+      <TournamentView />
     </div>
    )
   }
