59aeedb8b6ecf28f50d8043c135f3beb3c56a22d
ArbutusNate <arbutusnate@gmail.com>
2017-12-05 11:55:29 -0800

    making different components for tournament structures. Working on bestof5

---
+++ client/src/components/BestOfFive/BestOfFive.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './BestOfFive.css';
+
+const BestOfFive = props =>
+  <div className="b5-background">
+    <div className="b5-header">
+      <h2>Best 3 of 5 Tournament</h2>
+    </div>
+    <div className="b5-main">
+
+    </div>
+  </div>
+
+export default BestOfFive;
--- client/src/components/MainView/MainView.js
+++ client/src/components/MainView/MainView.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Header from '../Header';
 import Social from '../Social';
-import TournamentView from '../TournamentView';
+import BestOfFive from '../BestOfFive';
 import './MainView.css';
 
 class MainView extends Component {
@@ -10,7 +10,7 @@ class MainView extends Component {
     <div className="background">
       <Header />
       <Social />
-      <TournamentView />
+      <BestOfFive />
     </div>
    )
   }
