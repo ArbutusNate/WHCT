d44f8fea7bf6d521ae0fb272525ad5ca275c1608
ArbutusNate <arbutusnate@gmail.com>
2017-12-07 12:27:28 -0800

    added helper.js, still not finding routes module

---
+++ models/Tournaments.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema
+
+var Tournament = new Schema({
+
+
+
+
+})
+
+var Tournament = mongoose.model("Tournament", TournamentSchema);
+
+module.exports = Tournament;
