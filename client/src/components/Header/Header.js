4bfd4adbf3e28f9234912fbd57600c762fb2158c
ArbutusNate <arbutusnate@gmail.com>
2018-02-09 14:38:19 -0800

    login now working properly, css tweaks on sign in module to make it look nicer

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -9,16 +9,35 @@ const Header = props =>
         <h1> Community Tournaments </h1>
     </div>
     <nav>
-      <div className="nav-button" view="tournament" onClick={props.handleViewChange}>
+      <div
+        className="nav-button"
+        view="tournament"
+        onClick={props.handleViewChange}
+      >
         Current Tournaments
       </div>
-      <div className="nav-button" view="records" onClick={props.handleViewChange}>
+
+      <div
+        className="nav-button"
+        view="records"
+        onClick={props.handleViewChange}
+      >
         Records
       </div>
 
       {props.isLoggedIn ?
-        <button className="nav-button" onClick={() => this.props.logOut}> Log Out </button> :
-        <button className="nav-button" onClick={() => props.showHideModal('block')}> Log In </button>}
+        <button
+          className="nav-button"
+          onClick={() => this.props.logOut}
+        >
+           Log Out
+        </button> :
+        <button
+          className="nav-button"
+          onClick={() => props.showHideModal('block')}
+        >
+          Log In
+        </button>}
     </nav>
   </div>
 
