b9f8f228c39abcea1a7d2206dec93bad5f1e2097
ArbutusNate <arbutusnate@gmail.com>
2018-02-09 12:17:20 -0800

    firebase auth added

---
+++ client/src/firebase/firebase.js
@@ -0,0 +1,21 @@
+import * as firebase from 'firebase';
+
+var config = {
+  apiKey: "AIzaSyALPY4qVzrFZPqv9hYKPF8SqziPsrRECsM",
+  authDomain: "wildhawgstournam-1517688075718.firebaseapp.com",
+  databaseURL: "https://wildhawgstournam-1517688075718.firebaseio.com",
+  projectId: "wildhawgstournam-1517688075718",
+  storageBucket: "wildhawgstournam-1517688075718.appspot.com",
+  messagingSenderId: "759309936970"
+};
+
+if(!firebase.apps.length) {
+  firebase.initializeApp(config);
+}
+
+//export all Firebase endpoints as auth.
+const auth =  firebase.auth();
+
+export {
+  auth,
+}
