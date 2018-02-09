e97dafb806857af69aa374e171f3657b7bed8520
ArbutusNate <arbutusnate@gmail.com>
2018-02-09 13:11:57 -0800

    possibly fixed auth issue, not sure exactly what the problem is - something with firebase.js, the prod and dev versions, and ENOENT errors

---
+++ client/src/firebase/firebase.js
@@ -0,0 +1,27 @@
+import * as firebase from 'firebase';
+
+var config = {
+  //API_KEY
+  apiKey: process.env.API_KEY,
+  //AUTH_DOMAIN
+  authDomain: process.env.AUTH_DOMAIN,
+  //DATABASE_URL
+  databaseURL: process.env.DATABASE_URL,
+  //PROJECT_ID
+  projectId: process.env.PROJECT_ID,
+  //STORAGE_BUCKET
+  storageBucket: process.env.STORAGE_BUCKET,
+  //SENDER_ID
+  messagingSenderId: process.env.SENDER_ID
+};
+
+if(!firebase.apps.length) {
+  firebase.initializeApp(config);
+}
+
+//export all Firebase endpoints as auth.
+const auth =  firebase.auth();
+
+export {
+  auth,
+}
