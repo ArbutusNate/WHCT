f4d07a5a4ca20e5210ecdd6f9d025c265336d446
ArbutusNate <arbutusnate@gmail.com>
2018-02-09 12:34:54 -0800

    testing local and heroku login

--- client/src/firebase/firebase.js
+++ client/src/firebase/firebase.js
@@ -1,18 +1,19 @@
+
 import * as firebase from 'firebase';
 
 var config = {
   //API_KEY
-  apiKey: process.env.API_KEY,
+  apiKey: "AIzaSyALPY4qVzrFZPqv9hYKPF8SqziPsrRECsM",
   //AUTH_DOMAIN
-  authDomain: process.env.AUTH_DOMAIN,
+  authDomain: "wildhawgstournam-1517688075718.firebaseapp.com",
   //DATABASE_URL
-  databaseURL: process.env.DATABASE_URL,
+  databaseURL: "https://wildhawgstournam-1517688075718.firebaseio.com",
   //PROJECT_ID
-  projectId: process.env.PROJECT_ID,
+  projectId: "wildhawgstournam-1517688075718",
   //STORAGE_BUCKET
-  storageBucket: process.env.STORAGE_BUCKET,
+  storageBucket: "wildhawgstournam-1517688075718.appspot.com",
   //SENDER_ID
-  messagingSenderId: process.env.SENDER_ID
+  messagingSenderId: "759309936970"
 };
 
 if(!firebase.apps.length) {
